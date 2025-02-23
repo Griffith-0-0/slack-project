f0292a967d1ab4dca384eec42d84ee3f4054eae3
badr khafif <ba_khafif@etu.enset-media.ac.ma>
2025-02-23 14:00:29 +0000

    Google auth

--- src/app/features/auth/components/ui/Sign-in-card.tsx
+++ src/app/features/auth/components/ui/Sign-in-card.tsx
@@ -17,9 +17,12 @@ export const SingInCard = ({setState}: SingInCardProps) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [pending, setPending] = useState(false);
 
-    const handleProviderSignIn=(value: "github" | "google")=>{
-        signIn(value);
+    const onProviderSignIn=(value: "github" | "google")=>{
+        setPending(true);
+        signIn(value)
+            .finally(()=>setPending(false))
     }
 
     return (
@@ -34,15 +37,15 @@ export const SingInCard = ({setState}: SingInCardProps) => {
             </CardHeader>
             <CardContent className="space-y-5 px-0 pb-0">
                 <form className="space-y-2.5">
-                    <Input disabled={false} value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email" type="email" required/>
-                    <Input disabled={false} value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password" type="password" required/>
-                    <Button disabled={false} type="submit" size="lg" className="w-full">Continue</Button>
+                    <Input disabled={pending} value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email" type="email" required/>
+                    <Input disabled={pending} value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password" type="password" required/>
+                    <Button disabled={pending} type="submit" size="lg" className="w-full">Continue</Button>
                 </form>
                 <Separator/>
                 <div className="flex flex-col gap-y-2.5">
-                    <Button disabled={false} onClick={()=>{}} variant="outline" size="lg" className="w-full relative">
+                    <Button disabled={pending} onClick={()=>onProviderSignIn("google")} variant="outline" size="lg" className="w-full relative">
                         <FcGoogle className="size-5 absolute top-3 left-2.5"/>Continue with Google</Button>
-                    <Button disabled={false} onClick={()=>handleProviderSignIn("github")} variant="outline" size="lg" className="w-full relative">
+                    <Button disabled={pending} onClick={()=>onProviderSignIn("github")} variant="outline" size="lg" className="w-full relative">
                         <FaGithub className="size-5 absolute top-3 left-2.5"/>Continue with Github</Button>
                 </div>
                 <p className="text-xs text-muted-foreground">Don&apos;t have an account?
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {Button} from "@/components/ui/button";
 import {useAuthActions} from "@convex-dev/auth/react";
 
